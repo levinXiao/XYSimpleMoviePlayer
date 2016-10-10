9f55da7536c1f895529dac0cedcbd3995ab8146e
xiaoyu <xiaoyu@xiaoyudemacbook-pro.local>
2016-10-10 17:02:46 +0800

    Initial Commit

---
+++ XYSimpleMoviePlayerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XYSimpleMoviePlayerExample
+//
+//  Created by xiaoyu on 2016/10/10.
+//  Copyright © 2016年 xiaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
